4f515da329106a2d7d7528062bcfc7df8c4efab7
NHower <howernickoli@gmail.com>
2022-11-21 15:44:03 -0500

    Downgraded react router to v5.2.1, totally different from 6.4.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 import VendingMachine from './VendingMachine';
+import CandyBar from './CandyBar';
+import Chips from './Chips';
+import Pretzels from './Pretzels';
+
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <VendingMachine />
+      <BrowserRouter>
+        <Route exact path='/'>
+          <VendingMachine />
+        </Route>
+        <Route exact path='/chips'>
+          <Chips />
+        </Route>
+        <Route exact path='/candybar'>
+          <CandyBar />
+        </Route>
+        <Route exact path='/pretzels'>
+          <Pretzels />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
